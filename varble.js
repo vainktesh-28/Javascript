dc47de0ba502cc38e1dab3cf425bccec4bba8f9a
VAINKTESH SINGH <iamvs9587@gmail.com>
2024-09-21 19:48:41 +0530

    Basic javascript

---
+++ varble.js
@@ -0,0 +1,7 @@
+const age = 22;
+let firstName = "vainktesh";
+let lastName = "singh";
+
+let email = "iamvs9587@gmail.com";
+console.table([firstName , lastName , age , email]);
+console.log(firstName)
