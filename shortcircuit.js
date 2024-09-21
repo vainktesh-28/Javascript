7446370ba48c342a05dab0b87d8b8d728205d065
VAINKTESH SINGH <iamvs9587@gmail.com>
2024-09-21 19:56:09 +0530

    Logical operator

--- shortcircuit.js
+++ shortcircuit.js
@@ -3,3 +3,9 @@ console.log(0 && 40);
 console.log(10 && null);
 console.log("vainktesh" && 0);
 console.log("" && 0);
+
+
+console.log(10 || "");
+console.log(0 || -10);
+console.log(0 || "vainktesh");
+console.log(10 || null);
