62ccd8e3bb798fc65e91d29a917fb0a91c302f17
VAINKTESH SINGH <iamvs9587@gmail.com>
2024-09-22 22:33:32 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,19 @@
+let a = [ 10, "Vishal", true, 20];
+console.log(a);
+console.log(typeof(a));
+
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+
+// Updating indices in array
+
+a[1] = "Vainktesh";
+console.log(a);
+
+let arr = new Array(4);
+console.log(arr);
+
+let arr2 = new Array(5).fill(2);
+console.log(arr2);
---
+++ arrays2.js
@@ -0,0 +1,19 @@
+let arr = [2, 3, 4 ];
+console.log(arr.length);
+
+arr.push(10); // Add an element at last
+console.log(arr); 
+
+arr.pop(10);  // Remove an element at last
+console.log(arr); 
+
+arr.unshift(5); // Add an element in start
+console.log(arr); 
+
+arr.shift(5); // Remove an element in start
+console.log(arr); 
+
+arr.reverse();  // Reverse an array
+console.log(arr)
+
+console.log(arr.indexOf(4));
